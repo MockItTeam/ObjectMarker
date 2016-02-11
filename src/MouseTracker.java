4291c4c34f9d9b8a8043a3a2da2779e3b5ffad21
mapfap <mapfaps@gmail.com>
2016-02-11 14:01:48 +0700

    Refactor marking result calculation logic

--- src/MouseTracker.java
+++ src/MouseTracker.java
@@ -3,24 +3,23 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class MouseTracker extends MouseAdapter {
-	
-	
+
 	private Point firstCoor;
 	private Point lastCoor;
 	private CoordinateLabel coorLabel;
-	
+
 	public MouseTracker(CoordinateLabel coorLabel) {
 		this.coorLabel = coorLabel;
 	}
-	
+
 	@Override
 	public void mousePressed(MouseEvent e) {
 		firstCoor = new Point(e.getX(), e.getY());
 	}
-	
+
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		
+
 		lastCoor = new Point(e.getX(), e.getY());
 		calculateMarking();
 	}
@@ -29,61 +28,57 @@ public class MouseTracker extends MouseAdapter {
 	public void mouseMoved(MouseEvent e) {
 		updateCoordinate(e);
 	}
-	
+
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		
+
 		updateCoordinate(e);
 	}
-	
+
 	private void updateCoordinate(MouseEvent e) {
 		coorLabel.setCoordinate(e.getX(), e.getY());
 		coorLabel.repaint();
 	}
-	
+
 	private void calculateMarking() {
 		int x, y, width, height;
 		int x0, y0, x1, y1;
-		
+
 		x0 = (int) firstCoor.getX();
 		y0 = (int) firstCoor.getY();
-		
+
 		x1 = (int) lastCoor.getX();
 		y1 = (int) lastCoor.getY();
-		
-		
-		if (x0 == x1 || y0 == y1) {
-			System.out.println("Empty Area");
-			return;
-		}
-		
-		// flip box.
+
+		// Drag from {left to right || right to left}
 		if (x0 < x1) {
 			x = x0;
 			width = x1 - x0;
-			if (y0 < y1) {
-				// Dragging from top-left to bottom-right
-				y = y0;
-				height = y1 - y0;
-			} else { // y0 > y1
-				// Dragging from bottom-left to top-right	
-				y = y1;
-				height = y0 - y1;
-			}
-		} else { // x0 > x1
+		} else if (x0 > x1) {
 			x = x1;
 			width = x0 - x1;
-			if (y0 < y1) {
-				// Dragging from bottom-right to top-left
-				y = y0;
-				height = y1 - y0;
-			} else { // y0 > y1
-				// Dragging from top-right to bottom-left
-				y = y1;
-				height = y0 - y1;
-			}
+		} else {
+			x = x0;
+			width = 0;
+		}
+
+		// Drag from {top to bottom || bottom to top}
+		if (y0 < y1) {
+			y = y0;
+			height = y1 - y0;
+		} else if (y0 > y1) {
+			y = y1;
+			height = y0 - y1;
+		} else {
+			y = y0;
+			height = 0;
 		}
 		
-		System.out.println(String.format("%s %s %s %s", x, y, width, height));
+		if (width == 0 || height == 0) {
+			System.out.println("Empty area.");
+		} else {
+			System.out.println(String.format("%s %s %s %s", x, y, width, height));			
+		}
+
 	}
 }
